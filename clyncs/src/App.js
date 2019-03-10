127672f022bf259e1848fd34e22bedcf3bb7f588
Huthman King <kinghuthman@gmail.com>
2019-03-10 18:40:39 -0400

    Under Construction

--- clyncs/src/App.js
+++ clyncs/src/App.js
@@ -4,19 +4,17 @@ import TopBar from './components/TopBar/TopBar';
 import HomepageHeader from './components/Homepage/HomepageHeader';
 import Homepagecontent from './components/Homepage/HomepageContent';
 import {Route, NavLink} from 'react-router-dom';
+import UnderConstruct from './components/Homepage/UnderConstruction';
 
 
 class App extends Component{
-  
-  
-  
 
     render () {
     return (
       <div className="App">
-      <HomepageHeader />
-      <Homepagecontent />
-      <Route path="/home"  component={App} loc="https://www.luvvvnomore.com"  />
+      {/* <HomepageHeader />
+      <Homepagecontent /> */}
+      <Route path="/"  component={UnderConstruct} />
       
       </div>
     );
---
+++ clyncs/src/components/Homepage/UnderConstruction.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import infinite from './infinitelinks4.jpg';
+
+const UnderConstruct = props => {
+  return (
+        <div className="center">
+        <img className="infinite" src={infinite} />
+        <h1>UNDER CONSTRUCTION</h1>
+        </div>
+  )
+}
+
+
+
+export default UnderConstruct;
